37ecf221fab72f5ced2358052edc6d3a771f51cd
Sergey An <ansergeyg@gmail.com>
2021-08-08 00:31:39 +0200

    Fenwick Tree init

---
+++ DataStructures/FenwickTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FenwickTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here will be implementation of the Fenwick Tree - Binary Indexed Tree (BIT)");
+        }
+    }
+}
